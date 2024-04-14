085f2f6bb737127629da502cc906a880847f93ad
Snayderm <sneyder1002m@gmail.com>
2024-04-14 13:56:47 -0500

    Servicio AppLogger

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,16 +1,2 @@
-﻿﻿HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
-
-Builder.Services.AddNorthWindServices();
-
-using IHost AppHost = Builder.Build();
-
-
-IProductService Service = AppHost.Services.GetRequiredService<IProductService>();
-Service.Add("Demo", "Azúcar Refinada"); 
-
-/*     IMPLEMENTACIONES DE TRES PRINCIPIOS IMPORTANTES
- * AppLogger y los Writers: Responsabilidad Única
- * AppLogger: Abierto pero Cerrado
- * AppLogger: Inversión de dependencias. Los módulos de alto nivel
- * son independientes de los detalles de implementación
- * */
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
