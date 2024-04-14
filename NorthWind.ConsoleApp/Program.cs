71e5a04daa4986f2a5964a0a2ec6c33b44117253
Snayderm <sneyder1002m@gmail.com>
2024-04-14 14:10:43 -0500

    Vista de los datos de Console al almacenamiento Output

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿using NorthWind.ConsoleApp.Services;
 using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
 
-IUserActionWriter Writer = new ConsoleWriter();
+IUserActionWriter Writer = new DebugWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application Started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azúcar Refinada");
--- NorthWind.Writers/DebugWriter.cs
+++ NorthWind.Writers/DebugWriter.cs
@@ -1,11 +1,11 @@
 ﻿namespace NorthWind.Writers
 {
-    internal class DebugWriter : IUserActionWriter
-    {
-        public void Write(UserAction action)
-        {
-            Debug.WriteLine("ConsoleWriter: {0}, {1}, {2}",
-                action.CreatedDateTime, action.User, action.Description);
-        }
-    }
+	public class DebugWriter : IUserActionWriter
+	{
+		public void Write(UserAction action)
+		{
+			Debug.WriteLine("ConsoleWriter: {0}, {1}, {2}",
+				action.CreatedDateTime, action.User, action.Description);
+		}
+	}
 }
